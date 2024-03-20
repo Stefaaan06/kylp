65c8ba090eae8fa7c2c24d6a62775dee9f76df07
felix <felix20.09@hotmail.com>
2024-03-20 17:25:24 +0100

    CMAKE WUP

---
+++ cmake-build-debug/CMakeFiles/kylp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kylp.dir/main.cpp.o"
+  "CMakeFiles/kylp.dir/main.cpp.o.d"
+  "kylp"
+  "kylp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kylp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
